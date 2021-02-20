204c42d9f33b9cea6d38a9cb95c0806170f6742a
Kordzakhiaa <giorgi.kordzakhia.2@btu.edu.ge>
2021-02-20 22:27:14 +0400

    added db.column in ecommerce models

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -16,7 +16,13 @@ class Ticket(models.Model):
 class Order(models.Model):
     user = models.ForeignKey(to='user.User', on_delete=models.SET_NULL, null=True, related_name=_('Orders'))
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name=_('Orders'))
-    created_date = models.DateTimeField(auto_now=True, verbose_name=_("Created Date"))
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name=_("Created Date"))
+    price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name=_("Price"), default=None)
 
     def __str__(self):
         return f"{self.user}'s order"
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.price = self.ticket.price
+        super(Order, self).save(*args, **kwargs)
